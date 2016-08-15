11103a6192261d60359a72cc1f41ebb405c0d6af
Martin Panik <martin.w.panik@gmail.com>
2016-08-15 22:10:48 +0200

    pridanie __get() do kontrolera

--- src/SimpleMvc/Controller/AbstractController.php
+++ src/SimpleMvc/Controller/AbstractController.php
@@ -213,4 +213,11 @@ abstract class AbstractController
         $this->layout = $layoutName;
     }
 
+    public function __get($name){
+        if (array_key_exists($name, $this->data)){
+            return $this->data[$name];
+        }
+        return null;
+    }
+
 }
